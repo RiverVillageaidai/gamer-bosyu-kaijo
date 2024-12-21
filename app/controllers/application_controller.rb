9a2d235e6e0b35311ddfb4d9539e984198e635bb
EC2 Default User <ec2-user@ip-172-31-33-82.eu-north-1.compute.internal>
2024-12-21 05:58:13 +0000

    [Update]ヘッダー動的な

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,13 @@
 class ApplicationController < ActionController::Base
+
+  # devise利用の機能（ユーザ登録、ログイン認証など）が使われる前にconfigure_permitted_parametersメソッドが実行される記述
+  before_action :configure_permitted_parameters, if: :devise_controller?
+
+  protected
+
+  # ユーザーログイン時にname を許可
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:name])
+  end
+
 end
