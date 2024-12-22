bf7db9ca3816325dd3240adc65de9d8753b463ce
RiverVillageaidai <kishiayasu@gmail.com>
2024-12-22 05:48:46 +0000

    [Add]マイページへのリンク

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,11 @@
 Rails.application.routes.draw do
-  get 'users/show'
-  get 'users/edit'
+
   devise_for :users
- root to: 'homes#top'
+
+  root to: 'homes#top'
   get 'homes/about'
 
-   resources :users, only: [:show, :edit, :update]
+  resources :users, only: [:show, :edit, :update]
 
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
