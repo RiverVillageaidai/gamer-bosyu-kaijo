4a39c785d7d3ebf347433d9636f388d8c08f6e08
RiverVillageaidai <kishiayasu@gmail.com>
2024-12-23 10:31:34 +0000

    [Add]post ルーティング

--- config/routes.rb
+++ config/routes.rb
@@ -1,15 +1,12 @@
 Rails.application.routes.draw do
 
-  get 'posts/new'
-  get 'posts/index'
-  get 'posts/show'
-  get 'posts/edit'
+
   devise_for :users
 
   root to: 'homes#top'
   get 'homes/about'
 
   resources :users, only: [:show, :edit, :update]
-
+  resources :posts, only: [:index, :show, :new, :create, :edit, :update, :destroy]
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
