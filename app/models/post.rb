97701c9bdaedc10a56b47b8e91f0d874feee55e0
RiverVillageaidai <kishiayasu@gmail.com>
2024-12-25 09:54:43 +0000

    [Fix]post_index  投稿画像表示

--- app/models/post.rb
+++ app/models/post.rb
@@ -9,9 +9,9 @@ class Post < ApplicationRecord
   def get_image(width, height)
     unless image.attached?
       file_path = Rails.root.join('app/assets/images/no_image.jpg')
-      profile_image.attach(io: File.open(file_path), filename: 'default-image.jpg', content_type: 'image/jpeg')
+      image.attach(io: File.open(file_path), filename: 'default-image.jpg', content_type: 'image/jpeg')
     end
-      profile_image.variant(resize_to_limit: [width, height]).processed
+      image.variant(resize_to_limit: [width, height]).processed
   end
 
 end
