e5ce8fa68a6c8c8c3a0f057198eb572c081c8793
RiverVillageaidai <kishiayasu@gmail.com>
2024-12-26 10:03:35 +0000

    [Add]投稿編集機能

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -27,10 +27,24 @@ class PostsController < ApplicationController
   end
 
   def edit
+    post = Post.find(params[:id])
+    unless post.user_id == current_user.id
+      redirect_to post_path(post.id)
+    end
+    @post = post
   end
 
   def update
-    
+    @post = Post.find(params[:id])
+    if @post.update(post_params)
+      #  "flash[:notice] = 'メッセージ内容'"　リクエスト間を超えてメッセージを表示するための仕組み
+      flash[:notice] = "You have updated user successfully."
+      flash[:alert] = ""
+      redirect_to post_path(@post.id)
+    else
+      
+      render :edit
+    end
   end
 
   def destroy
