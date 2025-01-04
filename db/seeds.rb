fd35f89626275d964849457b220e7afa9b48374e
RiverVillageaidai <kishiayasu@gmail.com>
2025-01-04 07:42:22 +0000

    [Add]テストデータ

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,19 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+olivia = User.find_or_create_by!(email: "olivia@example.com") do |user|
+  user.name = "Olivia"
+  user.password = "password"
+  user.birthday = ""
+  user.introduction = ""
+  user.profile_image = ActiveStorage::Blob.create_and_upload!(io: File.open("#{Rails.root}/db/fixtures/sample-user1.jpg"), filename:"sample-user1.jpg")
+  user.is_public = "TRUE"
+  user.is_active = "TRUE"
+end
+
+Post.find_or_create_by!(title: "(test)ランクが上がりました") do |post|
+  post.body = "(test)２年間やり続けたかいあってとうとうランクが上がりました。"
+  post.image = ActiveStorage::Blob.create_and_upload!(io: File.open("#{Rails.root}/db/fixtures/sample-post1.jpg"), filename:"sample-post1.jpg")
+  post.user = olivia
+end
