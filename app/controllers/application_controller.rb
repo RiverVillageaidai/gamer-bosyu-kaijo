dad3eadfa404036f132792e3471d36c488b47b6e
RiverVillageaidai <kishiayasu@gmail.com>
2025-01-07 10:39:14 +0000

    [Add]非ログインユーザーのアクセス制限

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,6 +3,9 @@ class ApplicationController < ActionController::Base
   # devise利用の機能（ユーザ登録、ログイン認証など）が使われる前にconfigure_permitted_parametersメソッドが実行される記述
   before_action :configure_permitted_parameters, if: :devise_controller?
 
+  # ログインしていないユーザーは top とabout以外にアクセスするとログイン画面に推移する
+  before_action :authenticate_user!, except: [:top, :about]
+
   protected
 
   # ユーザーログイン時にname を許可
