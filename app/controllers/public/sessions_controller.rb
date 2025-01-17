301944938ddd1056f5f107b0cc9fd18bda2d407d
RiverVillageaidai <kishiayasu@gmail.com>
2025-01-17 10:19:05 +0000

    [Add]admin  and public

--- app/controllers/public/sessions_controller.rb
+++ app/controllers/public/sessions_controller.rb
@@ -1,7 +1,15 @@
 # frozen_string_literal: true
 
-class Users::SessionsController < Devise::SessionsController
+class Public::SessionsController < Devise::SessionsController
   # before_action :configure_sign_in_params, only: [:create]
+
+  
+  # devise利用の機能（ユーザ登録、ログイン認証など）が使われる前にconfigure_permitted_parametersメソッドが実行される記述
+  before_action :configure_permitted_parameters, if: :devise_controller?
+
+  before_action :reject_user, only: [:create]
+
+ 
 
   # GET /resource/sign_in
   # def new
@@ -18,9 +26,12 @@ class Users::SessionsController < Devise::SessionsController
   #   super
   # end
 
-  before_action :reject_user, only: [:create]
-
    protected
+
+  # If you have extra params to permit, append them to the sanitizer.
+  # def configure_sign_in_params
+  #   devise_parameter_sanitizer.permit(:sign_in, keys: [:attribute])
+  # end
 
   def reject_user
     @user = User.find_by(email: params[:user][:email]) #params[:モデル名][:カラム名]受け取ったemailに一致するユーザーを User モデルから検索
@@ -35,8 +46,9 @@ class Users::SessionsController < Devise::SessionsController
     end
   end
 
-  # If you have extra params to permit, append them to the sanitizer.
-  # def configure_sign_in_params
-  #   devise_parameter_sanitizer.permit(:sign_in, keys: [:attribute])
-  # end
+    # ユーザーログイン時にname を許可
+    def configure_permitted_parameters
+      devise_parameter_sanitizer.permit(:sign_up, keys: [:name])
+    end
+    
 end
--- config/routes.rb
+++ config/routes.rb
@@ -9,28 +9,30 @@ Rails.application.routes.draw do
     resources :users, only: [:destroy]
   end
 
-  devise_for :users, controllers: { sessions: 'users/sessions' }
+  scope module: :public do
+    devise_for :users, controllers: { sessions: 'public/sessions' } #Deviseのデフォルトのsessionsコントローラーをカスタムコントローラー(public/sessions)に置き換え
 
-  root to: 'homes#top'
+    root to: 'homes#top'
   
-  get 'homes/about'
+    get 'homes/about'
 
-  #検索ボタンが押された時、Searchesコントローラーのsearch実行
-  get "search" => "searches#index"
+    #検索ボタンが押された時、Searchesコントローラーのsearch実行
+    get "search" => "searches#index"
 
-  resources :users, only: [:show, :edit, :update, :index] do
-    member do
-      # ユーザー退会確認画面
-      get 'deactivate' => 'users#deactivate'
-      # ユーザー退会　論理削除
-      patch 'withdraw' => 'users#withdraw'
+    resources :users, only: [:show, :edit, :update, :index] do
+     member do
+        # ユーザー退会確認画面
+       get 'deactivate' => 'users#deactivate'
+        # ユーザー退会　論理削除
+       patch 'withdraw' => 'users#withdraw'
+     end
     end
-  end
 
-  resources :posts, only: [:index, :show, :new, :create, :edit, :update, :destroy] do
-    resources :comments, only: [:create, :destroy]
-  end
+    resources :posts, only: [:index, :show, :new, :create, :edit, :update, :destroy] do
+      resources :comments, only: [:create, :destroy]
+    end
 
+  end
 
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
